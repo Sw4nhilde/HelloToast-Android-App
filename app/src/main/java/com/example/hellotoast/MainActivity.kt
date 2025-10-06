72df70b3f81a87282f6be1655b33c05961cd5ec9
EKA <eka220905@gmail.com>
2025-10-07 02:24:15 +0700

    Updated Commit

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -1,43 +1,55 @@
 package com.example.hellotoast
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Debug
-import android.os.PersistableBundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity () {
+class MainActivity : AppCompatActivity() {
     private var mCount = 0
     private lateinit var mShowCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mShowCount = findViewById(R.id.show_count)
 
         if (savedInstanceState != null) {
-            val isVisible = savedInstanceState.getBoolean("reply_visible")
-            if (isVisible) {
-                mCount = savedInstanceState.getInt("count_value")
-                mShowCount.text = mCount.toString()
-            }
+            mCount = savedInstanceState.getInt("count_value")
+            mShowCount.text = mCount.toString()
+        }
+
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
+        val buttonBrowser = findViewById<Button>(R.id.button_browser)
+
+        buttonSwitchPage.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        buttonBrowser.setOnClickListener {
+            val intentBrowse = Intent(Intent.ACTION_VIEW)
+            intentBrowse.data = Uri.parse("https://www.google.com")
+            startActivity(intentBrowse)
         }
     }
 
-    fun countUp (view: View) {
+    fun countUp(view: View) {
         mCount++
         mShowCount.text = mCount.toString()
     }
 
-    fun showToast (view: View) {
+    fun showToast(view: View) {
         Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean("reply_visible", true)
         outState.putInt("count_value", mCount)
     }
 }
